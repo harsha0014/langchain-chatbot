3e41ba10a777df15b0b2c16a3f5029b52d7a41f1
harsha0014 <161747279+harsha0014@users.noreply.github.com>
2025-01-23 01:45:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,47 @@
+import streamlit as st
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
 import os
 from dotenv import load_dotenv
-from langchain_community.llms import Ollama
-import streamlit as st
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser  
 load_dotenv()
+
 
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_PROJECT"]="Chatbot With Ollama"
+
 
 prompt=ChatPromptTemplate.from_messages(
     [
-        ("system","you are a helpfull assistant.please respond to the following questions"),
+        ("system","You are a helpful massistant . Please  repsond to the user queries"),
         ("user","Question:{question}")
     ]
 )
 
-st.title("Langchain Chatbot")
-input_text = st.text_input("Enter your question here:")
+def generate_response(question,temperature,max_tokens):
+    llm=Ollama(model="llama3.2:1b")
+    output_parser=StrOutputParser()
+    chain=prompt|llm|output_parser
+    answer=chain.invoke({'question':question})
+    return answer
 
 
-llm=Ollama(model="llama3.2:1b")
-output_parser=StrOutputParser()
-chain=prompt|llm|output_parser
+st.title("Q&A Chatbot With LLAMA")
 
-if input_text:
-    st.write(chain.invoke({"question":input_text}))
+temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
+max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=150)
+
+
+st.write("ask any question")
+user_input=st.text_input("You:")
+
+
+
+if user_input :
+    response=generate_response(user_input,temperature,max_tokens)
+    st.write(response)
+else:
+    st.write("Please provide the user input")
+
+
